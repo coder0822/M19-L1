9b6700a3154384e4587903245273625789ae157e
coder0822 <reshmasf22@gmail.com>
2024-09-18 15:48:25 +0530

    L1

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(P, Q):
+   return P+Q
+
+def subtract(P, Q):
+  return P - Q
+
+def multiply(P, Q):
+  return P * Q
+
+def divide(P, Q):
+ return P / Q
+# Now we will take inputs from the user
+print("Please select operation.")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiply")
+print("d. Divide")
+
+choice = input("Please enter choice (a/b/c/d):")
